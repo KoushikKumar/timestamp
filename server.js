d9c53b4d2b07167631e5ff8f9fd9c6349f727589
Koushik Kumar Vankadari <koushikkumar222@gmail.com>
2016-07-06 17:13:52 +0000

    upated port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var express = require("express"),
     routes = require(process.cwd()+"/app/routes/index.js");
+    
+var port = Number(process.env.port || 8080);
 
 var app = express();
 app.use('/public',express.static(process.cwd()+'/public'));
@@ -9,6 +11,6 @@ app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
 routes(app);
 
-app.listen(8080,function(){
-  console.log('listening to 8080');
+app.listen(port,function(){
+  console.log('listening to '+port);
 })
