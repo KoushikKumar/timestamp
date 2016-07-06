eba3d967c9d33b0128f9f29414e363e96c7e7f83
Koushik Kumar Vankadari <koushikkumar222@gmail.com>
2016-07-06 17:53:52 +0000

    updated to PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require("express"),
     routes = require(process.cwd()+"/app/routes/index.js");
     
-var port = Number(process.env.port || 8080);
+var port = Number(process.env.PORT || 8080);
 
 var app = express();
 app.use('/public',express.static(process.cwd()+'/public'));
